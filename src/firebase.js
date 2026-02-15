4c8ec677a89b5a311522cfbfd7b71b265ae13079
MARRANERA SEBASNUEL <raulfernando.rfdm@gmail.com>
2026-02-15 15:39:08 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "PEGA_AQUI_TU_APIKEY",
-  authDomain: "PEGA_AQUI.firebaseapp.com",
-  projectId: "PEGA_AQUI",
-  storageBucket: "PEGA_AQUI.appspot.com",
-  messagingSenderId: "PEGA_AQUI",
-  appId: "PEGA_AQUI",
+  apiKey: "AIzaSyB100c64z1C9EcBHi9NUK-C-qOu3n0ftWY",
+  authDomain: "marranera-sebasnuel.firebaseapp.com",
+  projectId: "marranera-sebasnuel",
+  storageBucket: "marranera-sebasnuel.firebasestorage.app",
+  messagingSenderId: "262344078710",
+  appId: "1:262344078710:web:375259fface100c1a93657",
+  measurementId: "G-4836FTLYCY",
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exportar la base de datos (Firestore)
 export const db = getFirestore(app);
-``
